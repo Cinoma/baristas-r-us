de63eac55c101572b44b6cfcc726bcb6be028a7e
Chris Inoma <84427950+Cinoma@users.noreply.github.com>
2021-10-02 17:36:30 -0500

    Added a React Router to navigate between Shopping Cart and Product Page.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@mui/material';
 import { ShoppingCart } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
@@ -11,13 +12,13 @@ const Navbar = ({ totalItems }) => {
         <>
             <AppBar position='fixed' className={classes.AppBar} color='inherit'>
                 <Toolbar>
-                    <Typography variant='h6' className={classes.title} color='inherit'>
+                    <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
                         <img src={logo} alt='Commerce.js' height='25px' className={classes.image} />
                         Baristas-R-Us
                     </Typography>
                     <div classname={classes.grow} />
                     <div className={classes.button}>
-                        <IconButton aria-label='Show cart items' color='inherit'>
+                        <IconButton component={Link} to='/cart' aria-label='Show cart items' color='inherit'>
                             <Badge badgeContent={totalItems} color='secondary'>
                                 <ShoppingCart />
                             </Badge>
